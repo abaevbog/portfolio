19ed7221888bec9b0fbe0aa450ca289c065573bd
Bogdan Abaev <abaevbog@grinnell.edu>
2019-09-19 20:44:33 -0500

    dynamic random picture

--- app/controllers/welcome_page_controller.rb
+++ app/controllers/welcome_page_controller.rb
@@ -1,5 +1,9 @@
 class WelcomePageController < ApplicationController
   def welcome
+    pic_to_pick = rand(1..3)
+    puts pic_to_pick
+    picture = "me_#{pic_to_pick}.jpg"
+    @picture = picture
   end
 end
  
