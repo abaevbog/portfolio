9f604ab50ebfdeca395de228a22857a33cd86da8
Bogdan Abaev <abaevbog@grinnell.edu>
2019-09-28 21:29:39 -0500

    H6

--- app/controllers/welcome_page_controller.rb
+++ app/controllers/welcome_page_controller.rb
@@ -1,9 +1,7 @@
 class WelcomePageController < ApplicationController
   def welcome
     pic_to_pick = rand(1..3)
-    puts pic_to_pick
-    picture = "me_#{pic_to_pick}.jpg"
-    @picture = picture
+    @picture = "me_#{pic_to_pick}.jpg"
   end
 end
  
--- test/controllers/welcome_page_controller_test.rb
+++ test/controllers/welcome_page_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
-
+require 'rails-controller-testing' 
+require 'set'
 class WelcomePageControllerTest < ActionDispatch::IntegrationTest
   test "should get welcome" do
     get welcome_page_welcome_url
     assert_response :success
   end
+
+  test "We get different picture" do 
+    x = 20
+    set = Set[]
+    for i in 0...20
+       get welcome_page_welcome_url
+       pic = assigns(:picture) 
+       set.add(pic)     
+    end
+
+    for x in ["me_1.jpg","me_2.jpg","me_3.jpg"] do
+      assert_includes set, x
+    end 
+  end
+
 
 end
