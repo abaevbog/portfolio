d0180bd74ea16b43899bad6c7f942e667ea0f869
Bogdan Abaev <abaevbog@grinnell.edu>
2019-11-03 15:33:30 -0600

    minor additions for the assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'welcome_page/welcome'
   get '/hackathons/', to: 'hackathons#show'
   get '/projects/', to: 'projects#show'
-  get '/kukusecretadmin/', to: 'visitors#show'
-  get '/kukusecretform', to: 'my_form#show'
-  get '/kukusecretmessage', to: 'messages#show'
+  get '/visitors/', to: 'visitors#show'
+  get '/leaveamessage/', to: 'my_form#show'
+  get '/messages/', to: 'messages#show'
   root 'welcome_page#welcome'
 end
  
